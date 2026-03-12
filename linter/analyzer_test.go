15ecaae931113c14e97ae6e168b8aa00ff5533c9
GeorgiiChertkoev <geochert13028@gmail.com>
2026-03-12 19:47:13 +0300

    fixed: linter/testdata is no longer ambigious

--- linter/analyzer_test.go
+++ linter/analyzer_test.go
@@ -1,18 +1,26 @@
 package linter
 
 import (
+	"os"
 	"path/filepath"
 	"testing"
 
 	"golang.org/x/tools/go/analysis/analysistest"
 )
 
+func testdataDir(t *testing.T) string {
+	t.Helper()
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+	return filepath.Join(wd, "testdata")
+}
+
 func TestAnalyzer(t *testing.T) {
-	testdata := filepath.Join("testdata")
-	analysistest.Run(t, testdata, Analyzer, ".")
+	analysistest.Run(t, testdataDir(t), Analyzer, ".")
 }
 
 func TestAnalyzerFixes(t *testing.T) {
-	testdata := filepath.Join("testdata")
-	analysistest.RunWithSuggestedFixes(t, testdata, Analyzer, ".")
+	analysistest.RunWithSuggestedFixes(t, testdataDir(t), Analyzer, ".")
 }
